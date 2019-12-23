bd3dafb9d4fe4a7f7ec95a08d27dd32ce0bf5c64
lanluoxiao <2320693692@qq.com>
2019-12-24 04:20:02 +0800

    改进文件结构

--- 排序.py
+++
@@ -1,5 +0,0 @@
-with open('字.yaml', 'r') as f:
-    l = sorted([line.strip('\n') for line in f], key = lambda x: len(x.split(':')[0]) * 1000000 + ord(x[0]))
-
-with open('字.yaml', 'w') as f:
-    f.write('\n'.join(l))
